62b4be88983383044125beba022daa4a10d0669d
Xrythidon <alexdanisz@live.com>
2019-12-03 01:53:38 -0500

    Added posts and comments

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -38,8 +40,8 @@ const Profile = ({
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            <div className="profile-exp bg-white p-2"> 
-              <h2 className="text-primary">Experience</h2> 
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
                   {profile.experience.map(experience => (
@@ -53,6 +55,25 @@ const Profile = ({
                 <h4>No experience credentials</h4>
               )}
             </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No Education credentials</h4>
+              )}
+            </div>
+
+            {profile.githubusername && (
+                <ProfileGithub username={profile.githubusername}/>
+            )}
           </div>
         </Fragment>
       )}
